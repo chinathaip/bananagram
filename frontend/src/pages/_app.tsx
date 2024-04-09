9016b47926931dd62d9531005bda088a033a2101
stiusyntaxclub <admin@stamford.dev>
2024-04-09 23:15:52 +0700

    add global font styling in _app.tsx

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,14 +1,27 @@
+import "@/styles/globals.css";
+
 import ApplicationShell from "@/components/core/application-shell";
 import { ThemeProvider } from "@/components/ui/theme-provider";
-import "@/styles/globals.css";
+import { fontSans } from "@/lib/fonts";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-			<ApplicationShell>
-				<Component {...pageProps} />
-			</ApplicationShell>
-		</ThemeProvider>
+		<>
+			{/* This... is not an elegant solution, but I have no idea how to style the sheet/popover/etc. */}
+			<style jsx global>{`
+				html {
+					font-family: ${fontSans.style.fontFamily};
+					-webkit-font-smoothing: antialiased;
+					-moz-osx-font-smoothing: grayscale;
+				}
+			`}</style>
+
+			<ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+				<ApplicationShell>
+					<Component {...pageProps} />
+				</ApplicationShell>
+			</ThemeProvider>
+		</>
 	);
 }
