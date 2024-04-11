67383173b0db21ba60ab934194cfa22089fab4a4
stiusyntaxclub <admin@stamford.dev>
2024-04-11 20:52:19 +0700

    remove title from _document https://nextjs.org/docs/messages/no-document-title

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,11 +4,12 @@ import ApplicationShell from "@/components/core/application-shell";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import { fontSans } from "@/lib/fonts";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
-			{/* This... is not an elegant solution, but I have no idea how to style the sheet/popover/etc. */}
+			{/* This... is not an elegant solution, but I have no idea how to set the font on sheet/popover/etc. */}
 			<style jsx global>{`
 				html {
 					font-family: ${fontSans.style.fontFamily};
@@ -16,6 +17,10 @@ export default function App({ Component, pageProps }: AppProps) {
 					-moz-osx-font-smoothing: grayscale;
 				}
 			`}</style>
+
+			<Head>
+				<title>Bananagram</title>
+			</Head>
 
 			<ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
 				<ApplicationShell>
