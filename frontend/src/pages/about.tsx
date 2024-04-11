426cf997c705bf66895560c783c3813a28b762c6
stiusyntaxclub <admin@stamford.dev>
2024-04-11 15:23:34 +0700

    add another h-screen div to about for testing

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -2,7 +2,8 @@ export default function AboutPage() {
 	return (
 		<div className="container mx-auto">
 			<div className="h-screen">1</div>
-			<div className="h-screen">1</div>
+			<div className="h-screen">2</div>
+			<div className="h-screen">3</div>
 		</div>
 	);
 }
