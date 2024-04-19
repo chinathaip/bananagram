ac3deca48836481a4b83453e1b3f9df662cf09b8
chinathaip <cartoonabe@gmail.com>
2024-04-20 02:09:27 +0700

    backend: set not null  and varchar size constraints

--- backend/data/init.sql
+++ backend/data/init.sql
@@ -11,13 +11,13 @@ CREATE TABLE public.user (
 
 CREATE TABLE public.category (
   id SERIAL PRIMARY KEY,
-  name VARCHAR UNIQUE
+  name VARCHAR(255) UNIQUE
 );
 
 CREATE TABLE public.post (
     id SERIAL PRIMARY KEY,
-    content VARCHAR,
-    user_id VARCHAR REFERENCES public.user (id) ON DELETE CASCADE,
+    content VARCHAR NOT NULL,
+    user_id VARCHAR(255) NOT NULL REFERENCES public.user (id) ON DELETE CASCADE,
     category_id INTEGER REFERENCES public.category (id),
     created_at TIMESTAMP NOT NULL DEFAULT now(),
     updated_at TIMESTAMP
@@ -27,14 +27,14 @@ CREATE TABLE public.comment (
     id SERIAL PRIMARY KEY,
     content VARCHAR,
     post_id INTEGER REFERENCES public.post (id),
-    user_id VARCHAR REFERENCES public.user (id),
+    user_id VARCHAR(255) REFERENCES public.user (id),
     created_at TIMESTAMP NOT NULL DEFAULT now(),
     updated_at TIMESTAMP
 );
 
 CREATE TABLE public.hashtag (
   id SERIAL PRIMARY KEY,
-  name VARCHAR UNIQUE
+  name VARCHAR(255) UNIQUE
 );
 
 INSERT INTO public.user (id, username, email, bio, display_name, profile_picture) VALUES ('user_2f2BNrbARuhvr1M84Jq4kALpw9O', 'chinathai', 'cartoonabe@gmail.com', '', '', 'https://images.clerk.dev/oauth_google/img_2f2BNtJlk61Ubm5YZfPLTAqUTVU');
