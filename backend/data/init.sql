2a93a05c6b1285060008d6840f966133a21651d7
chinathaip <cartoonabe@gmail.com>
2024-04-19 21:37:40 +0700

    insert mock data for development

--- backend/data/init.sql
+++ backend/data/init.sql
@@ -39,3 +39,20 @@ CREATE TABLE public.hashtag (
 
 INSERT INTO public.user (id, username, email, bio, display_name, profile_picture) VALUES ('user_2f2BNrbARuhvr1M84Jq4kALpw9O', 'chinathai', 'cartoonabe@gmail.com', '', '', 'https://images.clerk.dev/oauth_google/img_2f2BNtJlk61Ubm5YZfPLTAqUTVU');
 INSERT INTO public.user (id, username, email, bio, display_name, profile_picture) VALUES ('user_2f02EDTfrcAuyhODlRHaNLP6LQQ', 'tawan', 'alohasunshineday@gmail.com', '', '', 'https://images.clerk.dev/oauth_google/img_2f02ECClQ9noFSgkv8NHZpJmIDc');
+
+INSERT INTO public.category (name) VALUES ('Education');
+INSERT INTO public.category (name) VALUES ('Technology');
+INSERT INTO public.category (name) VALUES ('Entertainment');
+
+INSERT INTO public.hashtag(name) VALUES ('nextjs');
+INSERT INTO public.hashtag(name) VALUES ('javascript');
+INSERT INTO public.hashtag(name) VALUES ('frontend');
+INSERT INTO public.hashtag(name) VALUES ('universitylife');
+INSERT INTO public.hashtag(name) VALUES ('study');
+INSERT INTO public.hashtag(name) VALUES ('stamford');
+
+INSERT INTO public.post (content, user_id, category_id) VALUES ('How I get 0 hours of sleep because of classes','user_2f2BNrbARuhvr1M84Jq4kALpw9O', 1);
+INSERT INTO public.post (content, user_id, category_id) VALUES ('NextJS is great and all that....','user_2f02EDTfrcAuyhODlRHaNLP6LQQ', 2);
+
+INSERT INTO public.comment (content, post_id, user_id) VALUES ('Me too....', 1, 'user_2f02EDTfrcAuyhODlRHaNLP6LQQ');
+INSERT INTO public.comment (content, post_id, user_id) VALUES ('sure, but dockerize it yourself', 1, 'user_2f2BNrbARuhvr1M84Jq4kALpw9O');
