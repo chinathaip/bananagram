b8adb26c38d3b26a52602913ffa7d0678329cf4c
chinathaip <cartoonabe@gmail.com>
2024-04-20 00:16:23 +0700

    backend: implement createPost mutation

--- backend/src/post/dto/create-post.input.ts
+++ backend/src/post/dto/create-post.input.ts
@@ -1,7 +1,18 @@
-import { InputType, Int, Field } from '@nestjs/graphql';
+import { InputType, Int, Field } from "@nestjs/graphql";
+import { IsNotEmpty, IsNumber } from "class-validator";
 
 @InputType()
 export class CreatePostInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+	// TODO: maybe specify max length
+	@IsNotEmpty()
+	@Field()
+	content: string;
+
+	@IsNotEmpty()
+	@Field()
+	user_id: string;
+
+	@IsNumber()
+	@Field((type) => Int)
+	category_id: number;
 }
--- backend/src/post/post.service.ts
+++ backend/src/post/post.service.ts
@@ -1,15 +1,36 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, InternalServerErrorException, Logger } from "@nestjs/common";
 import { CreatePostInput } from "./dto/create-post.input";
 import { UpdatePostInput } from "./dto/update-post.input";
 import { Post } from "./entities/post.entity";
 import { DatabaseService } from "../db/db.service";
+import { QueryConfig } from "pg";
+import { BadRequestError } from "src/common/errors/bad-request.error";
 
 @Injectable()
 export class PostService {
 	constructor(private readonly db: DatabaseService) {}
 
-	create(createPostInput: CreatePostInput) {
-		return "This action adds a new post";
+	private readonly logger = new Logger(PostService.name);
+
+	async create(createPostInput: CreatePostInput): Promise<Post> {
+		try {
+			const queries: QueryConfig[] = [
+				{
+					name: `Create New Post for : ${createPostInput.user_id}`,
+					text: `INSERT INTO public.post (content, user_id, category_id) VALUES ($1,$2, $3) RETURNING *`,
+					values: [createPostInput.content, createPostInput.user_id, createPostInput.category_id]
+				}
+			];
+			const results = await this.db.transaction(queries);
+			const post = results[0].rows as Post[];
+			return post[0];
+		} catch (e) {
+			if (e.code == 23503) {
+				throw new BadRequestError(`User ${createPostInput.user_id} does not exist`);
+			}
+			this.logger.error(`error when creating new post: ${e}`);
+			throw new InternalServerErrorException();
+		}
 	}
 
 	async findAll(): Promise<Post[]> {
