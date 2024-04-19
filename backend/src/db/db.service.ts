6208b3c678dee270b8f95c515fcea964eb98041f
chinathaip <cartoonabe@gmail.com>
2024-04-20 00:15:09 +0700

    backend: properly handle db error

--- backend/src/db/db.service.ts
+++ backend/src/db/db.service.ts
@@ -9,24 +9,34 @@ export class DatabaseService {
 	private readonly logger = new Logger(DatabaseService.name);
 
 	async query<T>(sql: string): Promise<T> {
-		const res = await this.connection.query(sql);
-		this.logger.log(`command: ${sql} affected: ${res.rowCount} rows`);
-		return res.rows as T;
+		try {
+			const res = await this.connection.query(sql);
+			this.logger.log(`command: ${sql} affected: ${res.rowCount} rows`);
+			return res.rows as T;
+		} catch (e) {
+			this.logger.error(`error while querying: ${e}`);
+		}
 	}
 
 	async transaction(queries: QueryConfig[]): Promise<TransactionResult[]> {
+		const transactionResult: TransactionResult[] = [];
 		try {
-			const transactionResult: TransactionResult[] = [];
 			await this.connection.query("BEGIN");
-			queries.forEach(async (query) => {
-				const res = await this.connection.query(query);
-				transactionResult.push({ name: query.name, rowsAffected: res.rowCount, rows: res.rows });
-				this.logger.log(`transaction: ${query.name}, rows affected: ${res.rowCount}`);
-			});
+			await Promise.all(
+				queries.map(async (query) => {
+					try {
+						const res = await this.connection.query(query);
+						transactionResult.push({ name: query.name, rowsAffected: res.rowCount, rows: res.rows });
+						this.logger.log(`transaction: ${query.name}, rows affected: ${res.rowCount}`);
+					} catch (e) {
+						await this.connection.query("ROLLBACK");
+						throw e;
+					}
+				})
+			);
 			await this.connection.query("COMMIT");
 			return transactionResult;
 		} catch (e) {
-			await this.connection.query("ROLLBACK");
 			throw e;
 		}
 	}
