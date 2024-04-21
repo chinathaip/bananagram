7dec8e23089d11b16eb4f05aa2409bf91cb27661
stiusyntaxclub <admin@stamford.dev>
2024-04-21 20:47:09 +0700

    frontend: testing home page use-infinite-posts implementation

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,74 +1,50 @@
-import { usePost } from "@/lib/hooks/data-hooks/use-post";
-import { useSession } from "@clerk/nextjs";
-import { useEffect, useState } from "react";
-
-const examplePosts = [
-	{
-		id: "post123",
-		content:
-			"Lorem ipsum dolor sit **amet** consectetur, adipisicing elit. Quae hic tempore asperiores tenetur velit pariatur, iusto rem. Corrupti ex illum commodi, aut tenetur nisi magnam porro magni! Fugiat, mollitia? _Animi_ officiis numquam fugiat voluptates quasi laudantium reprehenderit totam dolores expedita! [Link](https://example.com)",
-		timestamp: new Date("2024-04-11T15:00:00Z"), // Example timestamp
-		username: "chinathai",
-		displayName: "ChinathaiP",
-		avatarUrl: "https://github.com/chinathaip.png",
-		avatarFallback: "CP",
-		canEdit: true,
-		canDelete: true
-	},
-	{
-		id: "post124",
-		content: "Lorem short! __Kek__",
-		timestamp: new Date("2024-04-11T15:00:00Z"), // Example timestamp
-		username: "shadcn",
-		displayName: "shad",
-		avatarUrl: "https://github.com/shadcn.png",
-		avatarFallback: "SC",
-		canEdit: false,
-		canDelete: false
-	}
-];
+import PostCard from "@/components/ui/post-card";
+import { useInfinitePosts } from "@/lib/hooks/data-hooks/use-infinite-posts";
+import { useIntersection } from "@mantine/hooks";
+import { useEffect } from "react";
 
 export default function Home() {
-	// const { isPending, error, data } = useRepoStats("TanStack/query");
+	const { data, error, isError, isPending, fetchNextPage, hasNextPage } = useInfinitePosts();
 
-	// if (error) return <div>Error: {error.message}</div>;
+	const { ref, entry } = useIntersection({
+		threshold: 0.1 // Adjust this threshold according to your preference for when the load should trigger
+	});
 
-	const [postId, setPostId] = useState(1);
-
-	const { data, isError, isPending } = usePost(postId);
-	const { isSignedIn, session, isLoaded } = useSession();
-
+	// Use effect to handle fetching next page
 	useEffect(() => {
-		if (isLoaded && isSignedIn) {
-			session
-				.getToken({
-					template: "supabase"
-				})
-				.then((token) => {
-					console.log(token);
-				});
+		if (entry?.isIntersecting && hasNextPage) {
+			fetchNextPage();
 		}
-	}, [session]);
+	}, [entry?.isIntersecting, hasNextPage, fetchNextPage]);
+
+	if (isError) return <div>Error: {error.message}</div>;
 
 	return (
 		<div className="container mx-auto grid grid-cols-12">
-			<div className="relative hidden md:col-span-3 md:block">
-				{/* TODO: add new post here, and in the header when going below the md breakpoint */}
-				This space is for stuff like "New Post" on Desktops. Will be sticky.
-			</div>
-			{/* TODO: don't forget to remove h-screen, make this scrollable since it's the main content */}
-			<div className="relative col-span-12 flex h-screen flex-col gap-y-2 md:col-span-9">
-				{/* <PostEditor />
+			<div className="relative hidden md:col-span-3 md:block">Stuff goes here</div>
+			<div className="relative col-span-12 flex flex-col gap-y-2 overflow-auto md:col-span-9">
+				<div className="flex flex-col gap-y-2">
+					{data?.pages.map((page, pageIndex) =>
+						page.posts.edges.map((edge, index) => {
+							const isLastElement =
+								index === page.posts.edges.length - 1 && pageIndex === data.pages.length - 1;
+							return (
+								// <Card key={edge.node.id} className="p-2" ref={isLastElement ? ref : null}>
+								// 	<p>{edge.node.user.username}</p>
+								// 	<Separator />
+								// 	{JSON.stringify(edge.node, null, 2)}
+								// </Card>
+								<PostCard key={edge.node.id} post={edge.node} ref={isLastElement ? ref : null} />
+							);
+						})
+					)}
 
-				{examplePosts.map((post) => (
-					<PostCard key={`post_card_` + post.id} post={post} />
-				))} */}
-
-				{/* <PostCardSkeleton /> */}
-				{/* <pre>{isPending ? "Loading..." : JSON.stringify(data, null, 2)}</pre> */}
-
-				<input type="number" value={postId} onChange={(e) => setPostId(parseInt(e.target.value))} />
-				<pre>{isPending ? "Loading..." : isError ? "ERR" : JSON.stringify(data, null, 2)}</pre>
+					<div className="mt-2 w-full text-center">
+						{isPending && "Loading..."}
+						{!hasNextPage && "You've reached the end!"}
+					</div>
+				</div>
+				{/* <button onClick={fetchNextPage}>{hasNextPage ? "Load More" : "You're all caught up!"}</button> */}
 			</div>
 		</div>
 	);
