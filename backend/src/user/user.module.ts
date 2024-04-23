910815f6449a9ce54c31455a256da026817ce2e6
chinathaip <cartoonabe@gmail.com>
2024-04-23 23:04:41 +0700

    backend: implement basic user graphql resolver

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -2,11 +2,12 @@ import { Module } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { UserController } from "./user.controller";
 import { DatabaseModule } from "../db/db.module";
+import { UserResolver } from "./user.resolver";
 
 @Module({
 	imports: [DatabaseModule],
 	controllers: [UserController],
-	providers: [UserService],
+	providers: [UserService, UserResolver],
 	exports: [UserService]
 })
 export class UserModule {}
--- backend/src/user/user.resolver.ts
+++ backend/src/user/user.resolver.ts
@@ -1,10 +1,22 @@
-import { Resolver } from "@nestjs/graphql";
+import { Query, Resolver, Args, ResolveField, Parent } from "@nestjs/graphql";
 import { User } from "./entities/user.entity";
 import { UserService } from "./user.service";
+import { UseGuards } from "@nestjs/common";
+import { JwtAuthGuardOptional } from "src/auth/auth-jwt.guard";
+import { CurrentUser } from "src/auth/decorators/current-user.decorator";
 
 @Resolver((of) => User)
 export class UserResolver {
 	constructor(private readonly userService: UserService) {}
 
-	// TODO: query for getting user information?
+	@UseGuards(JwtAuthGuardOptional)
+	@Query(() => User)
+	user(@Args("id") id: string) {
+		return this.userService.findOne(id);
+	}
+
+	@ResolveField(() => Boolean)
+	is_owner(@CurrentUser() userId: string, @Parent() user: User) {
+		return userId === user.id;
+	}
 }
