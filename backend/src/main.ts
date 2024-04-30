171702b31c0c49d5fbce03e998b4669f7dbdfd33
chinathaip <cartoonabe@gmail.com>
2024-05-01 02:58:09 +0700

    fix: cors not working

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule, {
-		cors: { origin: ["http://localhost:3000", "*"], credentials: true, preflightContinue: true }
+		cors: true
 	});
 	app.setGlobalPrefix("_api");
 	app.useGlobalPipes(new ValidationPipe());
