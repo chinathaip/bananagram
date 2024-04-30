e3d924b693ed0496c91d0ff5df27d435622bf3a8
chinathaip <cartoonabe@gmail.com>
2024-05-01 02:48:29 +0700

    backend: enable credentials for cors header

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from "./app/app.module";
 import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule, { cors: { origin: ["http://localhost:3000", "*"] } });
+	const app = await NestFactory.create(AppModule, {
+		cors: { origin: ["http://localhost:3000", "*"], credentials: true }
+	});
 	app.setGlobalPrefix("_api");
 	app.useGlobalPipes(new ValidationPipe());
 	await app.listen(3001);
