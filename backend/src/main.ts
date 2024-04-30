5423318ce6e2995962da7576d71b1a348a0a6ec8
chinathaip <cartoonabe@gmail.com>
2024-05-01 02:40:29 +0700

    backend: explicitly allow localhost frontend to access

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from "./app/app.module";
 import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule, { cors: { origin: "*" } });
+	const app = await NestFactory.create(AppModule, { cors: { origin: ["http://localhost:3000", "*"] } });
 	app.setGlobalPrefix("_api");
 	app.useGlobalPipes(new ValidationPipe());
 	await app.listen(3001);
