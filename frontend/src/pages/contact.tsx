61c1d95ad0f58e8572fa5b3342fa2d44c6f6c4f9
chinathaip <cartoonabe@gmail.com>
2024-06-04 13:16:02 +0700

    frontend: format contact page

--- frontend/src/pages/contact.tsx
+++ frontend/src/pages/contact.tsx
@@ -1,14 +1,13 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
 import { useState } from "react";
 
 export default function ContactPage() {
-	const [issue, setIssue] = useState("")
-	const [description, setDescription] = useState("")
+	const [issue, setIssue] = useState("");
+	const [description, setDescription] = useState("");
 	return (
 		<div className="flex min-h-screen w-full items-start justify-center pt-8">
 			<Card className="w-[350px]">
@@ -21,10 +20,11 @@ export default function ContactPage() {
 						<div className="grid w-full items-center gap-4">
 							<div className="flex flex-col space-y-1.5">
 								<Label htmlFor="your issue">Your Issue</Label>
-								<Select onValueChange={(selectedValue)=>{
-											setIssue(selectedValue);
-										}
-									}> 
+								<Select
+									onValueChange={(selectedValue) => {
+										setIssue(selectedValue);
+									}}
+								>
 									<SelectTrigger id="your issue">
 										<SelectValue placeholder="Select" />
 									</SelectTrigger>
@@ -37,15 +37,22 @@ export default function ContactPage() {
 							</div>
 							<div className="flex flex-col space-y-1.5">
 								<Label htmlFor="name">Please describe your issue</Label>
-								<Textarea onChange={e => setDescription(e.target.value)} placeholder="Describe the issue you are experiencing" />
+								<Textarea
+									onChange={(e) => setDescription(e.target.value)}
+									placeholder="Describe the issue you are experiencing"
+								/>
 							</div>
 						</div>
 					</form>
 				</CardContent>
 				<CardFooter className="flex justify-between">
-					<Button onClick={()=>{
-						window.open(`mailto:2009170008@students.stamford.edu?subject=${issue}&body=${description}`);
-					}}>Submit</Button>
+					<Button
+						onClick={() => {
+							window.open(`mailto:2009170008@students.stamford.edu?subject=${issue}&body=${description}`);
+						}}
+					>
+						Submit
+					</Button>
 				</CardFooter>
 			</Card>
 		</div>
